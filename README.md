2833cb67117b19ecd086123e328722e3d49cee15
Gus  Cantieni <gus.cantieni@gmail.com>
2025-01-23 12:53:42 -0500

    Add spotify playback and looping capability

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Track traditional tune collection
 
 Uses homebrew python3 installation and homebrew fzf.
+An python3 virtual environment is used and can be installed using the requirements.txt file.
 
 ### Commentary
 Ideally it should be quite easy to add a tune, and also quite easy to update the tune in the future. But getting in a tune that I want to be practicing seems like a quite desirable feature. Eventually I want to be able to do this from my phone with just a name.
@@ -22,6 +23,16 @@ This keeps me in the plaintext world which is convenient and nice.
 
 Though I do also want to store all of my audio files, potentially bare and potentially with tune.
 This also means that I should key not based on tune name but rather on a guid.
+
+The ultimate goal of this app is to actually practice tunes. To start I can export tunes into a flashcard app and a sheet music display app. Eventually I could integrate these features directly into the app, with a custom spaced repetition algorithm and built in abc notation viewer.
+
+Because it is an aural tradition, one of the most important features in practicing is the ability to playback audio. This will probably involve the spotify API for many of these tunes since I don't have a local copy for most of them, though this would be a good goal. I want to be able to loop a certain section of a spotify track.
+
+Spotify doesn't have a playback speed option, though there was this idea online:
+
+> If you have audio files or streams, use media players like VLC or custom players built with frameworks like Web Audio API (for web-based applications) that allow playback speed control.
+
+So perhaps I can try integrating the two. Or just learn tunes up to tempo for now
 
 ### features
 - [x] store tunes in a standard format
--- audio.py
+++ audio.py
@@ -2,8 +2,27 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 import os
+from time import sleep
+import threading
 
-def main():
+stop_loop = False
+
+def time_to_ms(time_str):
+    if time_str.find(":") != -1:
+        minutes, seconds = map(int, time_str.split(':'))
+    else:
+        minutes = 0
+        seconds = int(time_str)
+    return (minutes * 60 * 1000) + (seconds * 1000)
+
+class Track:
+    """An audio track."""
+    def __init__(self, uri, start=None, end=None):
+        self.uri = uri
+        self.start = time_to_ms(start) if start else start
+        self.end = time_to_ms(end) if end else end
+
+def connect_to_spotify():
     load_dotenv()
 
     client_id = os.getenv("SPOTIPY_CLIENT_ID")
@@ -13,8 +32,12 @@ def main():
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
                                                    client_secret=client_secret,
                                                    redirect_uri=redirect_uri,
-                                                   scope="user-library-read"))
+                                                   scope="user-modify-playback-state user-read-playback-state"))
     
+    return sp
+
+def list_artist_albums():
+    sp = connect_to_spotify()
     # example code from spotipy documentation
     # but with dallahan
     dallahan_uri = 'spotify:artist:1MfVe0OhbAVIhlXv5yrOUo'
@@ -27,6 +50,44 @@ def main():
     for album in albums:
         print(album['name'])
 
+def play_track(track):
+    sp = connect_to_spotify()
+    sp.start_playback(uris=[track.uri], position_ms=track.start)
+
+
+def listen_for_input():
+    global stop_loop
+    while True:
+        user_input = input("Press 'q' to quit: ").strip().lower()
+        if user_input == "q":
+            stop_loop = True
+            break
+
+def loop_track(track):
+    sp = connect_to_spotify()
+    sp.start_playback(uris=[track.uri], position_ms=track.start)
+
+    # Function to listen for user input
+    if track.end:
+        print("Looping track:")
+
+        input_thread = threading.Thread(target=listen_for_input, daemon=True)
+        input_thread.start()
+        
+        while True:
+            playback = sp.current_playback()
+            if not playback or stop_loop: #or not playback['is_playing']:
+                break  # Exit if playback stops
+
+            progress = playback['progress_ms']
+            if progress >= track.end:
+                # Restart playback at the loop start time
+                sp.start_playback(uris=[track.uri], position_ms=track.start)
+            sleep(0.5)  # Check playback position every 500ms
+
+def main():
+    john_dohertys = Track("spotify:track:3XDCKy6Z5lC9SP37doE9S5", start="11", end="32")
+    loop_track(john_dohertys)
 
 if __name__ == '__main__':
     main()
