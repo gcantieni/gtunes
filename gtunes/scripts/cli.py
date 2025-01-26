f4c2935ab7c751cefb81ebc83fdf44571e6584a7
Gus  Cantieni <gus.cantieni@gmail.com>
2025-01-26 14:52:47 -0500

    Add recs arg for searching recordings

--- gtunes/scripts/cli.py
+++ gtunes/scripts/cli.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 # The frontend: command line interface for the app.
 
-import shelve
 import argparse
+import shelve
 from gtunes import tune
 from gtunes import parse
 from gtunes import scrape
+from gtunes import db
 import csv
 import os
 
@@ -26,7 +27,7 @@ def list(args):
         for name in shelf:
             print(f"{shelf[name]}")
 
-def parse(args):
+def parse_(args):
     parser = parse.TuneListParser(args.infile)
     tunes = parser.parse()
 
@@ -57,6 +58,10 @@ def scrape_tunes(args):
     for abc in tune_abc:
         print(abc + "\n")
 
+# Find recordings for a particular tune and TODO save them to the tune database.
+def recs(args):
+    pass
+
 def main():
     parser = argparse.ArgumentParser(description="Add and manipulate traditional tunes.")
 
@@ -67,16 +72,9 @@ def main():
     parent_parser.add_argument("-l", type=str, default="tunes", dest="list_location", help="List location")
 
     # Create parsers for subcommands
+
     parser_add = subparsers.add_parser("add", parents=[parent_parser], help="Add a tune")
-    parser_parse = subparsers.add_parser("parse", parents=[parent_parser], help="Add list")
-    parser_list = subparsers.add_parser("list", parents=[parent_parser], help="List tunes")
-    parser_scrape = subparsers.add_parser("scrape")
-
     parser_add.set_defaults(func=add)
-    parser_list.set_defaults(func=list)
-    parser_parse.set_defaults(func=parse)
-    parser_scrape.set_defaults(func=scrape_tunes)
-
     parser_add.add_argument("name", type=str, help="Name of the tune")
     parser_add.add_argument("-t", type=str, dest="type", help="Type of tune (jig, reel, etc.)")
     parser_add.add_argument("-m", type=str, dest="mp3", help="Name of mp3 file")
@@ -85,15 +83,26 @@ def main():
     parser_add.add_argument("-a", type=str, dest="abc", help="ABC")
     parser_add.add_argument("-c", type=str, dest="comment", help="Comment(s)")
 
+    parser_list = subparsers.add_parser("list", parents=[parent_parser], help="List tunes")
+    parser_list.set_defaults(func=list)
     parser_list.add_argument("infile", help="Name of tune list")
     parser_list.add_argument("-n", dest="name", help="Name of tune")
     parser_list.add_argument("-t", dest="type", help="Type of tune (jig, reel, etc.)")
     parser_list.add_argument("-s", dest="status", help="Status of tune. How well the tune is known, int from 1-5.")
 
+    parser_parse = subparsers.add_parser("parse", parents=[parent_parser], help="Add list")
+    parser_parse.set_defaults(func=parse_)
     parser_parse.add_argument("infile", help="The path to the tune list to parse")
     parser_parse.add_argument("-o", dest="outfile", help="The name of the output file (db will be appended)")
 
+    parser_scrape = subparsers.add_parser("scrape")
+    parser_scrape.set_defaults(func=scrape_tunes)
     parser_scrape.add_argument("tune_name", help="Name of tune to find abc settings of.")
+
+    parser_recordings = subparsers.add_parser("recs", help="Find recordings of a tune and save your favorites.")
+    parser_recordings.set_defaults(func=recs)
+    parser_recordings.add_argument("tune", help="Name fo the tune to find recordings of.")
+
 
     args = parser.parse_args()
     if args.command:
